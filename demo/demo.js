eef77e944f5aa3dc5693a741e299f26a557b1ac2
addyosmani <addyosmani@gmail.com>
2012-05-28 18:18:55 +0100

    Expanding on demos.

--- demo/demo.js
+++ demo/demo.js
@@ -6,10 +6,15 @@ require({
     }
 });
 
-require(["hm!alpha"],
+require(["hm!alpha","lib/jquery"],
 function (alpha) {
-	console.log('Module content loaded:', alpha);
-	console.log(alpha.betaFunc());
-	console.log(alpha.gammaName);
-	console.log(alpha.epsilon.sigmaFunc())
+
+	var html = "";
+
+	$('.gamma').append(alpha.gammaName);
+	$('.beta').append(alpha.betaName);
+	$('.beta2').append(alpha.betaFunc());
+	$('.epsilon').append(JSON.stringify(alpha.epsilon));
+	$('.alpha').append(JSON.stringify(alpha));
+
 });
